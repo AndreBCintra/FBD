7bff2705fdfd44c9ad095d78eb540ec647c80153
AndreBCintra <49659483+AndreBCintra@users.noreply.github.com>
2019-12-01 22:00:47 -0300

    Create povoamento.sql

---
+++ povoamento.sql
@@ -0,0 +1,231 @@
+insert into Gravadora(cod_grav, nome, homepage, endereco)
+values (01, 'gravadora 1', 'www.grav1.com.br', 'Rua ex1')
+insert into Gravadora(cod_grav, nome, homepage, endereco)
+values (02, 'gravadora 2', 'www.grav2.com.br', 'Rua ex2')
+insert into Gravadora(cod_grav, nome, homepage, endereco)
+values (03, 'gravadora 3', 'www.grav3.com.br', 'Rua ex3')
+
+insert into Album
+values (10.00, 01, 'muito top', 01, '2001-12-3', '2019-4-6')
+insert into Album
+values (12.00, 02, 'muito top', 01, '1999-12-3', '2000-4-6')
+insert into Album
+values (10.90, 03, 'muito top', 01, '2001-12-3', '2012-4-6')
+insert into Album
+values (14.00, 04, 'muito top', 01, '2001-12-3', '2014-4-6')
+insert into Album
+values (10.00, 05, 'muito top', 02, '1999-01-01', '2019-4-6')
+insert into Album
+values (10.00, 06, 'muito top', 02, '2000-01-01', '2000-4-6')
+insert into Album
+values (10.00, 07, 'muito top', 03, '2001-12-3', '2019-4-6')
+insert into Album
+values (10.00, 08, 'muito top', 03, '2001-12-3', '2019-4-6')
+insert into Album
+values (10.00, 09, 'muito top', 02, '2001-12-3', '2019-4-6')
+
+insert into Tipo_Comp
+values (01, 'Sinfonia')
+insert into Tipo_Comp
+values (02, 'Opera')
+insert into Tipo_Comp
+values (03, 'Sonata')
+insert into Tipo_Comp
+values (04, 'Concerto')
+
+insert into Faixas
+values (1, 01, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (2, 02, 01, 'Nome da musica', 01, '00:02:24', 'DDD')
+insert into Faixas
+values (3, 03, 01, 'AAAAAAA', 01, '00:02:34', 'DDD')
+insert into Faixas
+values (4, 04, 02, 'What is the brother', 03, '00:04:27', 'ADD')
+insert into Faixas
+values (4, 05, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (5, 06, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (6, 07, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (7, 08, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (8, 09, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (9, 10, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (10, 11, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (11, 12, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (12, 13, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (13, 14, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (14, 15, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (15, 16, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (16, 17, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (17, 18, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (18, 19, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (19, 20, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (20, 21, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (21, 22, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (22, 23, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (23, 24, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (24, 25, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (25, 26, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (26, 27, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (27, 28, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (28, 29, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (29, 30, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (30, 31, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (31, 32, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (32, 33, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (33, 34, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (34, 35, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (35, 36, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (36, 37, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (37, 38, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (38, 39, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (39, 40, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (40, 41, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (41, 42, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (42, 43, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (43, 44, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (44, 45, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (45, 46, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (46, 47, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (47, 48, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (48, 49, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (49, 50, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (50, 51, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (51, 52, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (52, 53, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (53, 54, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (54, 55, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (55, 56, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (56, 57, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (57, 58, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (58, 59, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (59, 60, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (60, 61, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (61, 62, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (62, 63, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (63, 64, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (64, 65, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (65, 67, 01, 'muito top', 01, '00:02:34', 'ADD')
+insert into Faixas
+values (01, 68, 02, 'tp comp = concerto e per = barroco', 04, '00:02:34', 'ADD')
+
+insert into Per_Mus
+values (01, 'Barroco', '1500-1700?')
+insert into Per_Mus
+values (02, 'Idade Media', '1300-1500?')
+insert into Per_Mus
+values (03, 'Renascenca', '1700-1800?')
+insert into Per_Mus
+values (04, 'Classico', '1800-1850?')
+insert into Per_Mus
+values (05, 'Romantico', '1800-1900?')
+insert into Per_Mus
+values (06, 'Moderno', '1970-2020?')
+
+insert into Compositor
+values (01, 'Dvorack', 'Republica Tcheca', '1841-09-08', '1904-05-01', 05)
+insert into Compositor
+values (02, 'Ednaldo Pereira', 'Brasil', '1978-06-18', null, 06)
+insert into Compositor
+values (03, 'Alguem ai', 'Franca', '1501-09-08', '1504-05-01', 01)
+insert into Compositor
+values (04, 'so pra testar o bagulho do barroco la', 'barroco', '1841-09-08', '1904-05-01', 01)
+
+insert into Fai_Comp
+values (03, 01, 02)
+insert into Fai_Comp
+values (01, 01, 02)
+insert into Fai_Comp
+values (03, 01, 03)
+insert into Fai_Comp
+values (04, 02, 02)
+insert into Fai_Comp
+values (68, 02, 03)
+insert into Fai_Comp
+values (44, 01, 04)
+insert into Fai_Comp
+values (37, 01, 03)
+insert into Fai_Comp
+values (02, 01, 03)
+
+insert into Playlist
+values (01, 'Sertanejo 2019', getdate(), default)
+insert into Playlist
+values (02, 'só concerto barroco', getdate(), default)
+
+insert into Fai_Play
+values (02, 01, 01, default, default)
+insert into Fai_Play
+values (44, 01, 01, default, default)
+insert into Fai_Play
+values (68, 02, 02, default, default)
+insert into Fai_Play
+values (4, 02, 01, default, default)
+insert into Fai_Play
+--values (3, 01, 01, default, default)
+--insert into Fai_Play -- da errado pq é barroco ADD
+values (37, 01, 01, default, default)
+insert into Fai_Play
+values (02, 01, 02, default, default)
+insert into Fai_Play
+values (01, 01, 01, default, default)
+
