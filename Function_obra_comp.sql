d37b67ee780d030eca3d8b668d2e3c9f04cf4d00
AndreBCintra <49659483+AndreBCintra@users.noreply.github.com>
2019-12-02 13:36:47 -0300

    Update Function_obra_comp.sql

--- Function_obra_comp.sql
+++ Function_obra_comp.sql
@@ -1,12 +1,13 @@
 -- item 6
 create function f_obras_comp (@nome_comp varchar(50))
 returns @tab_alb_comp table
-(Código_do_Album smallint)
+(Nome varchar(50),
+Código_do_Album smallint)
 as
 begin
 insert into @tab_alb_comp
-select a.cod_alb from Album a, Faixas f, Fai_Comp fc, Compositor c
+select a.descricao, a.cod_alb from Album a, Faixas f, Fai_Comp fc, Compositor c
 where a.cod_alb = f.cod_alb and f.cod_fai = fc.cod_fai and fc.cod_comp = c.cod_comp and c.nome like '%'+@nome_comp+'%'
-group by a.cod_alb
+group by a.cod_alb, a.descricao
 return
 end
